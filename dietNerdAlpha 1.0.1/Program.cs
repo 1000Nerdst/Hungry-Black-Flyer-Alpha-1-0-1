fa0ece219e42897e801e9419a065e04a9b91f31e
Solomon West <1000nerdst@gmail.com>
2021-12-25 16:52:39 -0500

    Main Menu, Personal Setting and New Meal Plan is now operational

--- dietNerdAlpha 1.0.1/Program.cs
+++ dietNerdAlpha 1.0.1/Program.cs
@@ -17,7 +17,7 @@ namespace dietNerdAlpha_1._0._1
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new mainMenuForm());
         }
     }
 }
